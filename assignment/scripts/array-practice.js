585629da61906a65655eb5cf3c11db1d509bb966
Dane Petersen <dane.m.petersen@gmail.com>
2021-06-24 08:10:34 -0500

    added favorite food array and printed to the console

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -9,10 +9,13 @@ console.log('Animals are: ', animalArray);
 
 // 1.a. TODO: Make an array with some favorite foods
 
+let favFoods = ['pasta', 'meat and potatoes', 'chinese', 'mexican', 'indian']
+
 
 // 1.b. TODO: Log your array of foods to the console with a message, similar 
 //      to the example above
 
+console.log('My favorite foods are: ', favFoods);
 
 
 // 2. Array.length - tells you how many items are in the array
